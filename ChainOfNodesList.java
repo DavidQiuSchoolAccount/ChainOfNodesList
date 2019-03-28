d452e94001080d17100bb1116d17d034d781546b
DavidQiuSchoolAccount <dqiu00@stuy.edu>
2019-03-28 13:12:05 -0400

    Added add() and remove().

--- ChainOfNodesList.java
+++ ChainOfNodesList.java
@@ -17,12 +17,46 @@ public class ChainOfNodesList<E> implements Iterable<E> {
 		return true;
 	}
 
+	public boolean add(int index, E item) {
+		checkIndex(index);
+		Node<E> node = new Node<E>(item);
+		if(index == 0) { // Adding a new head.
+			node.next = head;
+			head = node;
+		} else  { // Adding in the middle.
+			Node<E> prev = getNode(index - 1);
+			node.next = prev.next;
+			prev.next = node;
+		} size++;
+		return true;
+	}
+
+	public E remove(int index) {
+		checkIndex(index);
+		E oldData;
+		if(index == 0) { // Removing the head.
+			oldData = head.data;
+			head = head.next;
+		} else { // Removing in the middle.
+			Node<E> prev = getNode(index - 1);
+			oldData = prev.next.data;
+			prev.next = prev.next.next;
+
+			// Check if we just removed the tail.
+			if(prev.next == null) { tail = prev; }
+		} size--;
+		return oldData;
+	}
+
 	public E get(int index) {
 		return getNode(index).data;
 	}
 
-	public void set(int index, E data) {
-		getNode(index).data = data;
+	public E set(int index, E data) {
+		Node<E> node = getNode(index);
+		E oldData = node.data;
+		node.data = data;
+		return oldData;
 	}
 
 	public ChainOfNodesIterator iterator() {
@@ -50,13 +84,16 @@ public class ChainOfNodesList<E> implements Iterable<E> {
 	}
 
 	private Node<E> getNode(int index) {
-		if(index < 0) { throw new NoSuchElementException("Index cannot be negative."); }
-		else if(index >= size) { throw new NoSuchElementException("Index cannot be greater than list size."); }
-
+		checkIndex(index);
 		Node<E> current = head;
 		for(int i = 0; i < index; i++) {
 			current = current.next;
 		} return current;
+	}
+
+	private void checkIndex(int index) {
+		if(index < 0) { throw new NoSuchElementException("Index cannot be negative."); }
+		else if(index >= size) { throw new NoSuchElementException("Index cannot reference beyond the list."); }
 	}
 
 	public class ChainOfNodesIterator implements Iterator<E> {
--- UserOfList.java
+++ UserOfList.java
@@ -4,15 +4,22 @@
 public class UserOfList {
 	public static void main(String[] args) {
 		ChainOfNodesList<String> letters = new ChainOfNodesList<>();
-		letters.add("A");
+		letters.add("1");
 		letters.add("B");
+		letters.add("3");
 		letters.add("X");
-		letters.set(2, "C");
-		letters.add("D");
+		letters.set(3, "C");
+		letters.add("E");
+		letters.add("5");
+		letters.remove(0);
+		letters.remove(1);
+		letters.remove(letters.size() - 1);
+		letters.add(2, "D");
+		letters.add(0, "A");
 
 		System.out.println("List contains " + letters.size() + " Strings.");
 		System.out.println("First two Strings are " + letters.get(0) + " and " + letters.get(1) + ".");
-		System.out.println(letters); // ["A", "B", "C", "D"]
+		System.out.println(letters);
 
 		for(String letter : letters) {
 			System.out.println(letter);
